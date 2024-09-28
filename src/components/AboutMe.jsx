3d1d66f189c082cb8eeea5e0d3bd7133239bf86f
Ryan Lei <135535159+RyanL3i@users.noreply.github.com>
2024-09-28 14:50:02 -0500

    updated resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,7 +14,7 @@ const AboutMe = () => {
         <div data-aos="fade-right" className="flex mt-8 gap-2">
           <div className="flex items-center justify-center">
             <div className="flex space-x-8">
-              <a href="https://firebasestorage.googleapis.com/v0/b/resume-4aaff.appspot.com/o/Resume%2FRyan%20Lei_%20Resume%20(1).pdf?alt=media&token=2d7f0fb3-cfaa-4892-9738-6cc69cd47b07" target="_blank">
+              <a href="https://firebasestorage.googleapis.com/v0/b/resume-4aaff.appspot.com/o/Resume%2FRyan%20Lei_%20Resume.pdf?alt=media&token=674e0979-8c09-43f3-a5a3-e60ead5d4bdb" target="_blank">
                 <button className="neon-button shadow-2xl hover:shadow-teal-300/50 text-white border-2 hover:bg-teal-300 hover:text-slate-800 border-teal-300
               rounded-lg py-4 px-8 uppercase relative overflow-hidden font-bold">Resume</button>
               </a>
